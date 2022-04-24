90f9804698cc9c5739a3559f658bea9eb399ae5e
EC2 Default User <ec2-user@ip-172-31-13-240.ap-northeast-1.compute.internal>
2022-04-24 05:26:46 +0000

    Laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Profileの登録</title>
-    </head>
-    <body>
-        <h1>Profileの登録</h1></h1>
-    </body>
-</html>
+@extends('layouts.profile')
+@section('title', 'プロフィールの新規作成')
+
+
+
+@section('content')
+    <div class="container">
+        <div class="low">
+            <div class="col-md-8 mx -auto">
+                <h1>My プロフィール</h1>
+            </div>
+        </div>
+    </div>
+@endsection
