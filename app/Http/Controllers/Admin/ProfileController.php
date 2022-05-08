fb353a09654dfa5434d1dc7030920bea567280d0
EC2 Default User <ec2-user@ip-172-31-13-240.ap-northeast-1.compute.internal>
2022-05-08 04:35:28 +0000

    laravel16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,6 +7,19 @@ use App\Profile;
 
 class ProfileController extends Controller
 {
+    public function index(Request $request)
+    {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+      //検索されたら検索結果を取得する
+       $posts = Profile::where('title', $cond_name)->get();
+      } else {
+      //それ以外は全てのニュースを取得する
+       $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+    }
+    
    public function add()
     {
         return view('admin.profile.create');
@@ -26,14 +39,37 @@ class ProfileController extends Controller
       return redirect('admin/profile/create');
     }
     
-    public function edit()
-    {
-        return view ('admin.profile.edit');
-    }
+    public function edit(Request $request) {
+      //Mews Modelからデータ取得
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
     
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    } //
+    public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      
+
+      unset($profile_form['_token']);
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+      return redirect('admin/profile/');
+  }
+  
+  public function delete(Request $request) {
+    //該当するNewsModel取得
+    $profile = Profile::find($request->id);
+    //削除する
+    $profile->delete();
+    return redirect('admin/profile/');
+  }
 }
 
